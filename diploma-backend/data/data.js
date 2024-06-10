73f20c31e4556a1dc4beeb0a466fb147bce8cf25
Dalmonker <gavrilovichdmitry1@gmail.com>
2024-06-10 14:36:40 +0300

    +

--- diploma-backend/data/data.js
+++ diploma-backend/data/data.js
@@ -456,7 +456,7 @@ Static является значением по умолчанию для все
 \`\`\`
 *Пример 2.1. Использования свойства position со значением static*
 
-![Logo](https://drive.google.com/file/d/1JpLAPWnU3YAFsETpobD6d0HIkL8IF2Mu/view?usp=sharing)
+![Logo](https://imgur.com/a/nCltAt2)
 *Рис. 2.1. Position: static*
 
 **Relative**
