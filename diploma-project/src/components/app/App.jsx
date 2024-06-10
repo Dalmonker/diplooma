e528fa75e6a452daeef20f4b93d05932e061879f
Dalmonker <gavrilovichdmitry1@gmail.com>
2024-06-10 13:43:45 +0300

    +

--- diploma-project/src/components/app/App.jsx
+++ diploma-project/src/components/app/App.jsx
@@ -6,6 +6,7 @@ import AdminLessonPage from "../pages/AdminLessonPage";
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<div>Беларусь</div>} />
       <Route path="/:lesson" element={<MainPage />} />
       <Route path="/admin" element={<AdminPage />} />
       <Route path="/admin/:lesson" element={<AdminLessonPage />} />
